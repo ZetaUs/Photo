ba0f938a5816b1248eab6367aad2c1a38c7c6008
Zeta <zeta@example.com>
2026-05-11 12:25:49 +0800

    update

--- git-up.bat
+++ git-up.bat
@@ -1,5 +1,6 @@
 @echo off
-git add .
-git commit -m "update"
-git push -u origin main
+E:\ZTX\Git\bin\git.exe pull origin main --rebase
+E:\ZTX\Git\bin\git.exe add .
+E:\ZTX\Git\bin\git.exe commit -m "update"
+E:\ZTX\Git\bin\git.exe push -u origin main
 pause
